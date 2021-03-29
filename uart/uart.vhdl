5ad39c7ab6746246df50ee32c7dce6f3681ce0d8
Christopher Patton <chpatton013@gmail.com>
2021-03-28 19:08:03 -0700

    Add docs for uart entity

--- uart/uart.vhdl
+++ uart/uart.vhdl
@@ -1,3 +1,32 @@
+--
+-- Generic implementation of a UART RX/TX entity with customizable baud rate,
+-- data-bit width, and stop-bit width. This implementation does not use a parity
+-- bit, so its use cannot be customized. In addition, this implementation
+-- expects to receive little-endian data chunks (LSB-first), and for the idle
+-- state on the serial line to be high.
+--
+-- Generics:
+--
+-- * g_baud_rate: Standard baud rates are 110, 300, 600, 1200, 2400, 4800, 9600,
+--   14400, 19200, 38400, 57600, 115200, 128000 and 256000 bits per second. By
+--   default, 9600 is commonly chosen for low-speed applications. Higher speeds
+--   may safely be used depending on the capabilities of your hardware.
+-- * g_data_width: Data-bit widths from 5 to 9 are valid, however 8 is the
+--   standard default.
+-- * g_stop_width: Stop-bit widths may be 1 or 2, however 1 is the standard
+--   default.
+--
+-- Ports:
+--
+-- * i_clock and i_reset: Shared between the RX and TX entity.
+-- * i_rx_*: Inputs to the RX component.
+-- * o_rx_*: Outputs of the RX component.
+-- * i_tx_*: Inputs to the TX component.
+-- * o_tx_*: Outputs of the TX component.
+--
+-- Refer to the RX and TX files for information about their implementations.
+--
+
 library ieee;
 use ieee.std_logic_1164.all;
 
